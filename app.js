49922b60dce79e10b21cab6dbdfbc241d846ae18
SachinthaGaurawa <gaurawasachintha@gmail.com>
2025-08-14 09:50:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -946,7 +946,7 @@ function init(){
   renderGrid();
   setupSearch();
   setupChips();
-  setupResponsiveToolbar(); 
+  setupResponsiveToolbar();
   setupHeroAnimation();
   setupLazyHero();
   updateAllFooterYears();

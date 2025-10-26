d9b6ef1f873c71eba3c07a2165c92644708805d7
SachinthaGaurawa <gaurawasachintha@gmail.com>
2025-10-27 01:37:02 +0530

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -559,6 +559,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+        function download(file, text) {
+
+            //creating an invisible element
+
+            let element = document.createElement('a');
+            element.setAttribute('href',
+                'data:text/plain;charset=utf-8, '
+                + encodeURIComponent(text));
+            element.setAttribute('download', file);
+            document.body.appendChild(element);
+            element.click();
+
+            document.body.removeChild(element);
+        }
+
+        // Start file download.
+
+        document.getElementById("btn")
+            .addEventListener("click", function () {
+                let text =
+                    document.getElementById("text").value;
+                let filename = "GFG.txt";
+
+                download(filename, text);
+            }, false);
+
 
 
 
